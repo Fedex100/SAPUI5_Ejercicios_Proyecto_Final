7a41be4f7dcb81701e20719d16fdcfad3ae1436f
fedex100 <miguez.35@gmail.com>
2021-10-31 20:04:34 +0000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/te/weatherApp/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.te.weatherApp.controller.MainView", {});
+});
