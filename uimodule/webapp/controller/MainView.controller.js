cbffed974940e24fa8afe02d78368fe0168cd811
fedex100 <miguez.35@gmail.com>
2021-11-01 01:28:54 +0000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/te/portfolio/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.te.portfolio.controller.MainView", {});
+});
